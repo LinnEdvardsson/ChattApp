4a7c33dd07bcc28097db35ac05a8ba00b9d8813a
Josefin Törner <josefintorner@gmail.com>
2024-11-07 12:27:36 +0100

    wrote sender-class code

--- src/Sender.java
+++ src/Sender.java
@@ -1,2 +1,37 @@
-public class Sender {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Sender implements ActionListener {
+
+    InetAddress myOwnAddress;
+    int port;
+    DatagramSocket socket;
+    String message;
+    JTextArea chatArea;
+
+    public Sender(String myOwnAddress, int port, JTextField textField, JTextArea chatArea) throws UnknownHostException {
+        this.myOwnAddress = InetAddress.getByName(myOwnAddress);
+        this.port = port;
+        this.message = textField.getText();
+        this.chatArea = chatArea;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            socket = new DatagramSocket();
+            byte[] messageInBytes = message.getBytes();
+            DatagramPacket packet = new DatagramPacket(messageInBytes, messageInBytes.length, myOwnAddress, port);
+            socket.send(packet);
+            chatArea.append(message);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
