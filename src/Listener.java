b9033b203a609981109c857f01ff669670e0c247
Linn Edvardsson <linn.edvardsson@yh.nackademin.se>
2024-11-07 12:37:49 +0100

    add actionlistener

--- src/Listener.java
+++ src/Listener.java
@@ -1,2 +1,37 @@
-public class Listener {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.*;
+import java.util.List;
+
+public class Listener implements ActionListener{
+
+    InetAddress myOwnAddress;
+    int port;
+    DatagramSocket socket;
+    String message;
+    JTextArea chatArea;
+
+    public Listener(String MyOwnAddress, int port, JTextField textField, JTextArea chatArea) {
+        this.myOwnAddress = InetAddress.getLoopbackAddress();
+        this.port = port;
+        this.message = textField.getText();
+        this.chatArea = chatArea;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        try(DatagramSocket socket = new DatagramSocket(1234, myOwnAddress);
+        ){
+            byte[] bytes = new byte[1024];
+            DatagramPacket packet = new DatagramPacket(bytes, bytes.length);
+
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+    }
 }
+
